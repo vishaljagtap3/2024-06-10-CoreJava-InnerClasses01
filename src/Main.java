de2d56cbc9716fe0db765c2ac7a7e338edce0420
vishal <vishal.jagtap@bitcode.in>
2024-07-04 16:05:23 +0530

    Non static inner classes

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Outer o1 = new Outer(10);
+        Outer.Inner in11 = o1.new Inner(100);
+        Outer.Inner in12 = o1.new Inner(1000);
+        o1.display();
+
+        in11.update();
+
+        in11.displayIn();
+        in12.displayIn();
+
+        System.out.println();
+
+        Outer o2 = new Outer(20);
+        Outer.Inner in21 = o2.new Inner(200);
+        Outer.Inner in22 = o2.new Inner(2000);
+
+        o2.display();
+        in21.displayIn();
+        in22.displayIn();
+
+        System.out.println("----------------------");
+
+        in11.display();
+        in12.display();
+        System.out.println();
+
+        in21.display();
+        in22.display();
+
+    }
+}
